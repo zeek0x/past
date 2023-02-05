3ea076e8120b1757567268c7ed8a531d87adac69
OHZEKI Naoki <0h23k1.n40k1@gmail.com>
2023-02-05 17:47:08 +0900

    past202004-open/b

--- past202004-open/b/main.py
+++ past202004-open/b/main.py
@@ -1,13 +1,1 @@
-s = input()
-o = {}
-for c in s:
-    if c in o:
-        o[c] += 1
-    else:
-        o[c] = 1
-mk, mv = '', 0
-for k, v in o.items():
-    if mv < v:
-        mk = k
-        mv = v
-print(mk)
+print(max(s := input(), key=s.count))
