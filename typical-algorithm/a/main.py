0eff6fb71b2757187ffc493bd49300a899ab64c2
OHZEKI Naoki <0h23k1.n40k1@gmail.com>
2023-02-22 21:36:35 +0900

    typical-algorithm/a

--- typical-algorithm/a/main.py
+++ typical-algorithm/a/main.py
@@ -1,13 +1,8 @@
+import bisect
+
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
 
-ok = N
-ng = -1
-while abs(ok-ng) > 1:
-    mid = (ok+ng) // 2
-    if A[mid] >= K:
-        ok = mid
-    else:
-        ng = mid
+ok = bisect.bisect_left(A, K)
 
 print(-1) if ok == N else print(ok)
