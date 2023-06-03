58bf9250c6c90b02d8e01a53627ef4f6d22aa9cb
zeek0x <0h23k1.n40k1@gmail.com>
2023-06-03 23:55:04 +0900

    abc296/d

--- abc296/d/main.py
+++ abc296/d/main.py
@@ -1,2 +1,11 @@
+import math
+
 N, M = map(int, input().split())
-ans = -1
+INF = 10**16
+ans = INF
+for a in range(1, int(math.sqrt(M)) + 1):
+    b = math.ceil(M / a)
+    if a <= N and b <= N:
+        ans = min(ans, a * b)
+
+print(ans) if ans != INF else print(-1)
